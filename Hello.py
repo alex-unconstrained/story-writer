50a3c857c416c8c2c5ce5dccbbfc8fe7a44fa81a
alex-unconstrained <149216641+alex-unconstrained@users.noreply.github.com>
2023-12-05 06:14:48 +0000

    v9

--- Hello.py
+++ Hello.py
@@ -11,30 +11,9 @@ client = OpenAI()
 # Load your OpenAI API key from an environment variable for security
 client.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Streamlit page properties
-st.set_page_config()
-
-# Streamit book properties
-stb.set_book_config(menu_title="streamlit_book",
-                    menu_icon="lightbulb",
-                    options=[
-                            "What's new on v0.7.0?",
-                            "Core Features",
-                            ],
-                    paths=[
-                          "pages/00_whats_new.py", # single file
-                          "pages/01 Multitest", # a folder
-                          ],
-                    icons=[
-                          "code",
-                          "robot",
-                          ],
-                    save_answers=True,
-                    )
-
 def generate_story(genre, level):
     try:
-        prompt = f"Write a {genre} story suitable for a {level} reading level, with keywords in **bold** for learning."
+        prompt = f"Write a {genre} story suitable for a {level} reading level."
         completion = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -61,11 +40,30 @@ def generate_image(prompt):
         st.error("An error occurred while generating the image. Please try again.")
         print(e)
 
+def write_story_to_files(story_text, genre):
+    story_pages = story_text.split("\n\n")  # Assuming each paragraph is a new page
+    chapter_paths = []
+    for i, page_text in enumerate(story_pages, start=1):
+        chapter_dir = f"pages/{i:02d}_chapter_{genre}"
+        os.makedirs(chapter_dir, exist_ok=True)
+        
+        # Write text
+        with open(f"{chapter_dir}/page.md", "w") as file:
+            file.write(page_text)
+        
+        # Generate image URL
+        image_url = generate_image(f"A scene depicting {page_text[:30]}...")
+        if image_url:
+            with open(f"{chapter_dir}/image_url.txt", "w") as file:
+                file.write(image_url)
+
+        chapter_paths.append(chapter_dir)
+    return chapter_paths
+
 def main():
     st.title('Story and Image Generator')
 
     genre = st.radio("Choose a Genre", ('Fantasy', 'Sci-Fi', 'Mystery'))
-
     level_description = {
         'A': 'Beginner (Ages 5-7)',
         'B': 'Intermediate (Ages 8-10)',
@@ -74,16 +72,20 @@ def main():
     level = st.selectbox("Select a Reading Level", options=level_description.keys(),
                          format_func=lambda x: f"{x} - {level_description[x]}")
 
-    if genre and level:
+     if genre and level:
         if st.button('Write'):
             with st.spinner('Generating your story...'):
                 story_text = generate_story(genre, level)
                 if story_text:
-                    story_pages = story_text.split("\n\n")  # Assuming each paragraph is a new page
-                    images = [generate_image(f"A scene depicting {line}") for line in story_pages]
-                    stb.multiselect_page(story_pages, images)
+                    chapter_paths = write_story_to_files(story_text, genre)
+                    for path in chapter_paths:
+                        with open(f"{path}/page.md", "r") as file:
+                            page_text = file.read()
+                        with open(f"{path}/image_url.txt", "r") as file:
+                            image_url = file.read().strip()
+                        st.markdown(page_text)
+                        st.image(image_url)
 
-# Responsive design considerations
 if __name__ == "__main__":
     st.set_page_config(layout="wide")
     main()
