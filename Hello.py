ba5367335dfe12d0805ca06959c20a7ca8a13ff9
alex-unconstrained <149216641+alex-unconstrained@users.noreply.github.com>
2023-12-05 05:49:46 +0000

    v4

--- Hello.py
+++ Hello.py
@@ -4,39 +4,47 @@ import uuid
 import time
 import pandas as pd
 import io
+import streamlit_book as stb
 
 client = OpenAI()
 
 # Load your OpenAI API key from an environment variable for security
 client.api_key = st.secrets["OPENAI_API_KEY"]
 
-
-
 def generate_story(genre, level):
     try:
-        # Formulate the prompt
-        prompt = f"Write a {genre} story suitable for a {level} reading level."
-        # Call the OpenAI API
+        prompt = f"Write a {genre} story suitable for a {level} reading level, with keywords in **bold** for learning."
         completion = client.chat.completions.create(
-          model="gpt-3.5-turbo",
-          messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-          ]
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
         )
         return completion.choices[0].message
     except Exception as e:
-        # Handle exceptions like API errors
         st.error("An error occurred while generating the story. Please try again.")
         print(e)
 
-def main():
-    st.title('Story Generator')
+def generate_image(prompt):
+    try:
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+        return response.data[0].url
+    except Exception as e:
+        st.error("An error occurred while generating the image. Please try again.")
+        print(e)
 
-    # Genre Selection with enhanced UI
+def main():
+    st.title('Story and Image Generator')
+
     genre = st.radio("Choose a Genre", ('Fantasy', 'Sci-Fi', 'Mystery'))
 
-    # Reading Level Selection with additional information
     level_description = {
         'A': 'Beginner (Ages 5-7)',
         'B': 'Intermediate (Ages 8-10)',
@@ -45,13 +53,14 @@ def main():
     level = st.selectbox("Select a Reading Level", options=level_description.keys(),
                          format_func=lambda x: f"{x} - {level_description[x]}")
 
-    # Write Button with conditional activation
     if genre and level:
         if st.button('Write'):
             with st.spinner('Generating your story...'):
-                story = generate_story(genre, level)
-                if story:
-                    st.text_area("Your Story", story, height=300)
+                story_text = generate_story(genre, level)
+                if story_text:
+                    story_pages = story_text.split("\n\n")  # Assuming each paragraph is a new page
+                    images = [generate_image(f"A scene depicting {line}") for line in story_pages]
+                    stb.multiselect_page(story_pages, images)
 
 # Responsive design considerations
 if __name__ == "__main__":
